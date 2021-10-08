7ee6c272c87498b6eeed11f9aef0ead120914ab9
hustle-dev <dlwoabsdk@gmail.com>
2021-10-08 20:37:09 +0900

    feat: Review String ex using RegExp

--- String/2.js
+++ String/2.js
@@ -1,22 +1,16 @@
-// 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
-// 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-// 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-
 const solution = s => {
   let recommended = s
     .toLowerCase()
-    .replace(/[^\w-_.]/g, '')
+    .replace(/[^a-z0-9-_.]/g, '')
     .replace(/\.{2,}/g, '.')
     .replace(/^\.|\.$/g, '')
     .replace(/^$/g, 'a')
-    .substring(0, 15)
+    .replace(/^.{16,}$/g, match => match.slice(0, 15))
     .replace(/\.$/g, '');
 
-  if (recommended.length <= 2) {
-    const ch = recommended[recommended.length - 1];
-    while (recommended.length <= 2) {
-      recommended += ch;
-    }
+  const ch = recommended[recommended.length - 1];
+  while (recommended.length <= 2) {
+    recommended += ch;
   }
 
   return recommended;
--- test.js
+++ test.js
@@ -1,25 +1,18 @@
 const solution = s => {
-  let recommended = s
+  const recommend = s
     .toLowerCase()
-    .replace(/[^\w-_.]/g, '')
+    .replace(/[^a-z0-9-_.]/g, '')
     .replace(/\.{2,}/g, '.')
     .replace(/^\.|\.$/g, '')
     .replace(/^$/g, 'a')
-    .substring(0, 15)
-    .replace(/\.$/g, '');
+    .replace(/^.{16,}$/g, match => match.slice(0, 15))
+    .replace(/\.$/g, '')
+    .replace(
+      /[\w]{1,2}/,
+      match => match + match[match.length - 1].repeat(3 - match.length)
+    );
 
-  if (recommended.length <= 2) {
-    const ch = recommended[recommended.length - 1];
-    while (recommended.length <= 2) {
-      recommended += ch;
-    }
-  }
-
-  return recommended;
+  return recommend;
 };
 
 console.log(solution('...!@BaT#*..y.abcdefghijklm'));
-console.log(solution('z-+.^.'));
-console.log(solution('=.='));
-console.log(solution('123_.def'));
-console.log(solution('abcdefghijklmn.p'));
